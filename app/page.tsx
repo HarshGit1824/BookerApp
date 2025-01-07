5dd1e4f7d7b65af4775eabdeaeb718d3bce05cb7
Harshal Thaware <125807305+HarshGit1824@users.noreply.github.com>
2025-01-07 14:33:02 +0000

    Card Component using shadcn/ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,30 @@
 import Image from "next/image";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function Home() {
   return (
-    <h1>Hello NextJs</h1>
+    <>
+      <div className="min-h-screen flex justify-center items-center">
+      <Card className="w-72 mx-auto ">
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+    </div>
+    </>
   );
 }
