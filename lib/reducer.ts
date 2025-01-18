02737826a351a26a748407ecf68e7157b2ba9926
Harshal Thaware <125807305+HarshGit1824@users.noreply.github.com>
2025-01-18 14:34:59 +0000

    Make reducers work again

--- lib/reducer.ts
+++ lib/reducer.ts
@@ -1,20 +1,14 @@
-interface Book {
-  id: string;
-  title: string;
-  author: string;
-  rating: number;
-}
-
+import { Book } from "@/types/interfaces";
 type Action =
   | { type: "add"; payload: { title: string; author: string } }
   | { type: "remove"; payload: string }
   | { type: "like"; payload: string }
   | { type: "dislike"; payload: string }
   | { type: "sort" };
-
 export const booksReducer = (state: Book[], action: Action): Book[] => {
   switch (action.type) {
     case "add":
+      console.log("adding book");
       const book: Book = {
         id: crypto.randomUUID(),
         title: action.payload.title,
@@ -23,20 +17,21 @@ export const booksReducer = (state: Book[], action: Action): Book[] => {
       };
       return [book, ...state];
     case "remove":
+      console.log("removing book");
+
       return state.filter((book) => book.id !== action.payload);
     case "like":
+      console.log("liking book");
+
       return state.map((book) =>
-        book.id === action.payload
-          ? { ...book, rating: book.rating + 1 }
-          : book
+        book.id === action.payload ? { ...book, rating: book.rating + 1 } : book
       );
     case "dislike":
       return state.map((book) =>
-        book.id === action.payload
-          ? { ...book, rating: book.rating - 1 }
-          : book
+        book.id === action.payload ? { ...book, rating: book.rating - 1 } : book
       );
     case "sort":
+      console.log("sorting books");
       return [...state].sort((a, b) => b.rating - a.rating);
     default:
       return state;
